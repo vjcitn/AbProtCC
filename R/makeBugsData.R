422c0f16b25d794b36ac729a9b51d7b1abec16fb
Vince Carey <stvjc@channing.harvard.edu>
2019-02-02 15:58:37 -0500

    updating with full example support

---
+++ R/makeBugsData.R
@@ -0,0 +1,31 @@
+#' create input for barca from a csvfile that has a particular structure
+#' @param csvfile character(1) path to a CSV file with specific layout, see note
+#' @param prefix character(1) used to prefix ".jagsdata" for output
+#' @param dodump logical(1) if true, will write data in jags format to [prefix].jagsdata
+#' @examples
+#' infile = system.file(paste0("/csv/", "rewriteV", ".csv") # use existing test file
+#' tdir = tempdir()
+#' od = getwd()
+#' setwd(tdir)
+#' makeBugsData(infile)
+#' readLines("test.jagsdata")
+#' setwd(od)
+#' @export
+makeBugsData = function (csvfile, prefix="test", dodump = TRUE) 
+{
+    sspec = read.csv(csvfile, stringsAsFactors = FALSE)
+    sspec = sspec[-nrow(sspec), ]
+    N = nrow(sspec)
+    A = sspec$Control.Maternal.Ab
+    A[seq(1, N, 4)] = sspec[seq(1, N, 4), 3]
+    str = rep(1:(N/4), each = 4)
+    Nstrat = N/4
+    Nsubg = 2
+    d = rep(c(1, 0, 0, 0), Nstrat)
+    if (dodump) 
+        dump(c("N", "Nstrat", "Nsubg", "str", "d", "A"), paste0(prefix, 
+            ".jagsdata"))
+    else list(N = N, Nstrat = Nstrat, Nsubg = Nsubg, str = str, 
+        d = d, A = A)
+}
+
